7c18438cad85c7efd78d6c3808d9c87eced8269a
Eddie Freeman <eddie@ninjasudo.com>
2014-07-03 12:01:17 -0700

    initial project

---
+++ MAF/MAF/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MAF
+//
+//  Created by Eddie Freeman on 7/3/14.
+//  Copyright (c) 2014 NinjaSudo Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MAF/MAFTests/MAFTests.m
@@ -0,0 +1,34 @@
+//
+//  MAFTests.m
+//  MAFTests
+//
+//  Created by Eddie Freeman on 7/3/14.
+//  Copyright (c) 2014 NinjaSudo Inc. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MAFTests : XCTestCase
+
+@end
+
+@implementation MAFTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
