052d1af369d4b0da1404b12487c48cf9748686db
Guy Morita <guy@hipmunk.com>
2014-07-15 17:17:54 -0700

    Transactions shell working. Haven't plugged in form functionality

---
+++ MAF/MAF/CreateTransactionTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  CreateTransactionTableViewCell.h
+//  MAF
+//
+//  Created by Guy Morita on 7/15/14.
+//  Copyright (c) 2014 NinjaSudo Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CreateTransactionTableViewCell : UITableViewCell
+
+@end
---
+++ MAF/MAF/CreateTransactionTableViewCell.m
@@ -0,0 +1,25 @@
+//
+//  CreateTransactionTableViewCell.m
+//  MAF
+//
+//  Created by Guy Morita on 7/15/14.
+//  Copyright (c) 2014 NinjaSudo Inc. All rights reserved.
+//
+
+#import "CreateTransactionTableViewCell.h"
+
+@implementation CreateTransactionTableViewCell
+
+- (void)awakeFromNib
+{
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
