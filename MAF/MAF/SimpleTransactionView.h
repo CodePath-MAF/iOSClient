ce654987326b5e81cc702366d29029f3b44a7031
Guy Morita <guy@hipmunk.com>
2014-07-22 17:11:33 -0700

    adding simple trans

---
+++ MAF/MAF/SimpleTransactionView.h
@@ -0,0 +1,13 @@
+//
+//  SimpleTransactionView.h
+//  MAF
+//
+//  Created by Guy Morita on 7/22/14.
+//  Copyright (c) 2014 NinjaSudo Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SimpleTransactionView : UIView
+
+@end
---
+++ MAF/MAF/SimpleTransactionView.m
@@ -0,0 +1,33 @@
+//
+//  SimpleTransactionView.m
+//  MAF
+//
+//  Created by Guy Morita on 7/22/14.
+//  Copyright (c) 2014 NinjaSudo Inc. All rights reserved.
+//
+
+#import "SimpleTransactionView.h"
+
+@implementation SimpleTransactionView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
