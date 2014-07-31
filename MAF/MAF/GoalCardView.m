581c2ee75a85f64f5a55f6a435e89194466a1147
Guy Morita <guy@hipmunk.com>
2014-07-30 17:35:47 -0700

    Got the core of the animations working
    Now just have to make the collection view side scrolling

--- MAF/MAF/GoalCardView.m
+++ MAF/MAF/GoalCardView.m
@@ -8,6 +8,9 @@
 
 #import "GoalCardView.h"
 #import "Utilities.h"
+#import "OpenSansLightLabel.h"
+#import <UICountingLabel.h>
+
 
 @interface GoalCardView ()
 
@@ -15,8 +18,11 @@
 @property (weak, nonatomic) IBOutlet UILabel *goalNameLabel;
 @property (weak, nonatomic) IBOutlet UILabel *paymentDueLabel;
 @property (weak, nonatomic) IBOutlet UILabel *paymentAmountLabel;
+@property (weak, nonatomic) IBOutlet UICountingLabel *percentCompleteLabel;
+
 
 @property (weak, nonatomic) IBOutlet UIImageView *dueImageView;
+
 
 @end
 
@@ -36,6 +42,7 @@
 - (void)setGoal:(Goal *)goal {
     _goal = goal;
     
+    
 #warning TODO add Milestone Update
     NSInteger newMilestoneCount = 0;
     self.milestoneLabel.text = [[NSString alloc] initWithFormat:@"%d OF %d", newMilestoneCount, self.goal.numPayments];
@@ -43,6 +50,13 @@
     self.paymentAmountLabel.text = [[NSString alloc] initWithFormat:@"$%0.2f", self.goal.paymentAmount];
    
     self.paymentDueLabel.text = [Utilities prettyMessageFromTargetDate:self.goal.goalDate withStartDate:[self.goal createdAt] withInterval:self.goal.paymentInterval];
+    
+    float percentComplete = self.goal.currentTotal / self.goal.amount;
+    self.percentCompleteLabel.format = @"%d%%";
+    self.percentCompleteLabel.method = UILabelCountingMethodEaseInOut;
+    [self.percentCompleteLabel countFrom:0 to:percentComplete*100 withDuration:1.0];
+    
+    [self addCircle:percentComplete];
 }
 
 - (void)updateColors {
@@ -50,6 +64,43 @@
         [self.dueImageView setImage:[UIImage imageNamed:@"time_red"]];
         [self.paymentDueLabel setTextColor:[UIColor redColor]];
     }
+}
+
+- (void)addCircle:(float)percentageComplete {
+    
+    // Set up the shape of the circle
+    int radius = 40;
+    CAShapeLayer *circle = [CAShapeLayer layer];
+    // Make a circular shape
+    circle.path = [UIBezierPath bezierPathWithRoundedRect:CGRectMake(0, 0, 2.0*radius, 2.0*radius)
+                                             cornerRadius:radius].CGPath;
+    // Center the shape in self.view
+    circle.position = CGPointMake(CGRectGetMidX(self.frame)-radius,
+                                  CGRectGetMidY(self.frame)-radius);
+    
+    // Configure the apperence of the circle
+    circle.fillColor = [UIColor clearColor].CGColor;
+    circle.strokeColor = [UIColor blackColor].CGColor;
+    circle.lineWidth = 5;
+    circle.strokeEnd = percentageComplete;
+    
+    // Add to parent layer
+    [self.layer addSublayer:circle];
+    
+    // Configure animation
+    CABasicAnimation *drawAnimation = [CABasicAnimation animationWithKeyPath:@"strokeEnd"];
+    drawAnimation.duration            = 1.0; // "animate over 10 seconds or so.."
+    drawAnimation.repeatCount         = 1.0;  // Animate only once..
+    
+    // Animate from no part of the stroke being drawn to the entire stroke being drawn
+    drawAnimation.fromValue = [NSNumber numberWithFloat:0.0f];
+    drawAnimation.toValue   = [NSNumber numberWithFloat:percentageComplete];
+    
+    // Experiment with timing to get the appearence to look the way you want
+    drawAnimation.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseIn];
+    
+    // Add the animation to the circle
+    [circle addAnimation:drawAnimation forKey:@"drawCircleAnimation"];
 }
 
 @end
